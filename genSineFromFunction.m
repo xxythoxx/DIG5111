276a6fea5b64a844b6ff6672db0948c9e178abe8
xxythoxx <greensilldavid@gmail.com>
2025-02-04 10:41:57 +0000

    updated from tutorial sheet 1

--- genSineFromFunction.m
+++ genSineFromFunction.m
@@ -1,7 +1,17 @@
-Fs = 1000;
-dur = 1;
-freq = 99;
-theta = 0;
-A = 0.5;
-sig = DaveSineWave(A, freq, Fs, dur, theta);
-sound(sig, Fs);
+Amp = 0.9;
+f = 441;
+fs = 44100;
+ts = 1/fs;
+t = 0:1/fs:(0.1-ts);
+sig = Amp*sin(2*pi*f*t);
+sound(sig,fs);
+plot(t,sig);
+audiowrite("sine1.wav",sig,fs);
+
+
+x = [ 1 2 3 4 ];
+y = [ 10 15 20 25 ];
+figure(1);
+plot(x,y,'g-*');
+figure(2);
+stem(x,y,'g-*');
