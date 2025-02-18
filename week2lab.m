c6909d629d7904fe929e45c8a65006a5d6ce5b28
xxythoxx <greensilldavid@gmail.com>
2025-02-18 14:53:31 +0000

    added edited wk2 lab files

--- week2lab.m
+++ week2lab.m
@@ -7,6 +7,7 @@ filename = 'piano_middle_C.wav';
 %Sig stores raw audio data in column;
 %Fs sampling frequency
 
+
 samples = [1,2*Fs];
 clear y Fs
 [y,Fs] = audioread (filename, 'native'); %reading audiodata, displaying size in cmd window
@@ -15,6 +16,7 @@ whos y %whos returns the number of bytes each variable occupies in the workspace
 %import data for 0-0.5s and 0.5-1s into sig1 and sig2
 sig1 = audioread(filename,[1,22050]);%amount of samples from 0-0.5s
 sig2 = audioread(filename,[22051,44100]);%amount of samples from 0.5-1s (removing the last plotted sample in sig1)
+
 
 
 
