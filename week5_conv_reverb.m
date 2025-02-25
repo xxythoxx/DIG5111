7b7c0e263526f298ed3dd2fb4a3a67c63cd40a0e
xxythoxx <greensilldavid@gmail.com>
2025-02-25 16:19:56 +0000

    started adding wet/dry, doesnt properly work

--- week5_conv_reverb.m
+++ week5_conv_reverb.m
@@ -3,10 +3,21 @@
 %making an impulse response from that file in corrolation to sampling
 %frequency
 
+disp(Fs)
+disp(Fsir)
+
 [ir2]= resample (ir,Fs, Fsir); % creating another IR where it resamples based off
 %of values from all IRs 
 
 y = conv(sig_pluck, ir2(:,1)); % convoluting the pluck audio signal in relation to the
 %new impulse response
-conv_pluck = y./max(abs(y)); %normalising the audio
-sound (conv_pluck,Fs)%playing the sound when code is executed
+conv_pluck = y./max(abs(y)) *0.2 ; %normalising the audio
+
+%adding wet/dry controls
+     %   wetLevel = 0.5;
+      %  y = y(1:length(sig_pluck));
+       % dry = sig_pluck;
+        %output = (1-wetLevel) * dry + wetLevel * y;
+%output = output/max(abs(output));
+
+sound (output,Fs)%playing the sound when code is executed
