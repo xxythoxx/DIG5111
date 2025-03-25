73a25b4d72a362d24f43b84cb3a357782b063b3b
xxythoxx <greensilldavid@gmail.com>
2025-03-25 15:44:48 +0000

    more files from wk 9 lab

--- assignment_test_functions.m
+++ assignment_test_functions.m
@@ -13,6 +13,12 @@ plot (t,Y); %plotting waveform
 filterDesigner %uses built in filter design app 
 %%Y = filter(b,a,x); % y is output, x is input signal, b is array of denom. coefficients, a is array of numer. coefficients
 
+%formatting for using spectrogram S = spectrogram(x,window,noverlap,nfft,fs) 
+%e.g spectrogram(chirpsig,128,120,128,10000);
+% would take file chirpsig, 128 is the window (128rectangular window which
+%does have spectral leakage),3rd argument (120) is overlap (moving over 8 
+%samples at a time), with the other 128 being framesize. 10000 is sample
+%freq.
 %% BIQUAD FILTER
 Fs = 44100; %specify sampling frequency
 fc = 2000; %specify cutoff frequency
---
+++ wk9_chirp_stft.m
@@ -0,0 +1,23 @@
+Fs=10000; %10kHz sample freq
+A=0.8;
+Ts=1/Fs; %sample intervals
+dur=1.5; %length in secs
+t=0:Ts:dur; 
+Theta=2*pi*(100+200*t+500*t.*t); %as time changes, frequency changes
+%when t = 0, its going to tell what start frequency is (so the 200 is
+%200Hz)
+%when t is = to end duration, itll tell us the end of chirp signal
+%(1000*1.5)+200
+%for example, sweeping from 20-20kHz over 3 seconds...
+%20000/20 = A VALUE
+%3/2
+% theta = 2*pi*(100+________________________
+
+chirpsig=A*sin(Theta); %makes the chirp signal
+audiowrite('mychirp1.wav',chirpsig,Fs); %writes chirp to an audio file,
+%taking calculations from chirpsig and applying them with the sampling
+%frequency
+
+
+%formatting for using spectrogram S = spectrogram(x,window,noverlap,nfft,fs) 
+%e.g spectrogram(chirpsig,128,120,128,10000); would take file chirpsig, 
